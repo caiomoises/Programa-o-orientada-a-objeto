2d3959c729b7938c72f56dec3f154f2e2cf4710a
Caio Moisés <caio.cavalcante@alunos.ufersa.edu.br>
2024-02-05 15:07:00 -0300

    Correções para tabuleiro

--- Arrays/dama/Tabuleiro.java
+++ Arrays/dama/Tabuleiro.java
@@ -8,14 +8,28 @@ public class Tabuleiro {
                 tabela[l][c] = '.';
             }
         }
+        // time preto ('o')
+        for (int l = 0; l <= 2; l++) {
+            for (int c = 1-l%2; c < tabela.length; c+=2) {
+                tabela[l][c] = 'o';
+            }
+        }
+
+         // time branco ('x')
+         for (int l = 5; l <= 7; l++) {
+            for (int c = 1-l%2; c < tabela.length; c+=2) {
+                tabela[l][c] = 'x';
+            }
+        }
     }
 
     public String toString() {
         String res = "";
         for (int l = 0; l < tabela.length; l++){
             for (int c = 0; c < tabela[l].length; c++) {
-                tabela[l][c] = '.';
+               res += tabela[l][c] + "  ";
             }
+            res += '\n';
         }
         return res;
     }
